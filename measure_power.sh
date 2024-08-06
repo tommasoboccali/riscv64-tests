4f9ff43b9833ff6be1d1c1332c14f4fffab0a078
Tommaso Boccali <tom@Tommasos-MacBook-Air.local>
2024-08-06 16:15:31 +0200

    add energy in J

--- measure_power.sh
+++ measure_power.sh
@@ -7,7 +7,7 @@ measure_host=192.168.139.121
 command="wget -qO- http://${measure_host}/rpc/Shelly.GetStatus|sed 's#switch:0#switch0#g'|jq '.switch0.apower'"
 command2="wget -qO- http://${measure_host}/rpc/Shelly.GetStatus|sed 's#switch:0#switch0#g'|jq '.switch0.aenergy.total'"
 #reset counters
-wget -qO- "http://${measure_host}/rpc/Switch.ResetCounters?id=0"
+wget -qO- "http://${measure_host}/rpc/Switch.ResetCounters?id=0" >& /dev/null
 
 delay=$1
 comment=$2
@@ -18,6 +18,7 @@ while [ True ]
 do
         pw=$(eval $command)
         en=$(eval $command2)
-	echo `date` `date "+%s"` $pw  W $en Wh
+        j=$(awk "BEGIN {print $en*3600; exit}"|sed "s#,#.#g")
+	echo `date` `date "+%s"` $pw  W $en Wh $j J
 	sleep $delay
 done	
