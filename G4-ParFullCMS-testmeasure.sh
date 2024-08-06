69bd879e1d6268e6789ede47890c08cb398cde71
Tommaso Boccali <Tommaso.Boccali@cern.ch>
2024-08-06 09:13:11 +0200

    first test for standard power measurement

---
+++ G4-ParFullCMS-testmeasure.sh
@@ -0,0 +1,26 @@
+G4Prefix=/cvmfs/datacloud.infn.it/repo/riscv64-pioneer/geant4/
+#G4Prefix=/scratch/G4/INSTALL
+
+source ${G4Prefix}/bin/geant4.sh
+
+exe=/cvmfs/datacloud.infn.it/repo/riscv64-pioneer/parfullcms//ParFullCMS
+prefix=G4_ParFullCMS
+
+events_per_thread=100
+config=parfullcms.mac
+
+echo Starting at `date`
+
+for th in  1 2 4 8 16 32 64
+do
+	export G4FORCENUMBEROFTHREADS=${th}
+	nevents=$(($events_per_thread * $th))
+	echo executing ParFullCMS at ${th} threads and $nevents events at `date`  `date '+%s'`
+	./measure_power.sh 1 "ParFullCMS_${th}" > measure_ParFullCMS_${th}.log &
+	mpid=$!
+	echo Measuring power with PID $mpid
+	cat $config | sed "s#NEVENTS#$nevents#g" > macro.run 
+	time ${exe} macro.run >& ${prefix}_${th}.log
+	kill $mpid
+done
+
