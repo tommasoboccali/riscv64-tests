aea8641172e84e2ed8988eaaa8f7a96a0ba7fad1
Tommaso Boccali <tom@Tommasos-MacBook-Air.local>
2024-08-06 14:58:38 +0200

    working with shelly - add energy

--- measure_power.sh
+++ measure_power.sh
@@ -5,6 +5,9 @@
 measure_host=192.168.139.121
 
 command="wget -qO- http://${measure_host}/rpc/Shelly.GetStatus|sed 's#switch:0#switch0#g'|jq '.switch0.apower'"
+command2="wget -qO- http://${measure_host}/rpc/Shelly.GetStatus|sed 's#switch:0#switch0#g'|jq '.switch0.aenergy.total'"
+#reset counters
+wget -qO- "http://${measure_host}/rpc/Switch.ResetCounters?id=0"
 
 delay=$1
 comment=$2
@@ -14,6 +17,7 @@ echo COMMENT $comment START
 while [ True ]
 do
         pw=$(eval $command)
-	echo `date` `date "+%s"` $pw  W
+        en=$(eval $command2)
+	echo `date` `date "+%s"` $pw  W $en Wh
 	sleep $delay
 done	
