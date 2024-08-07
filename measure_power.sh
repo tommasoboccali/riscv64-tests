192a91d42829da942d3151d7ed08843ccc45df37
Tommaso Boccali <Tommaso.Boccali@cern.ch>
2024-08-07 11:21:23 +0200

    add cpu

--- measure_power.sh
+++ measure_power.sh
@@ -17,16 +17,17 @@ echo COMMENT $comment START
 
 while [ True ]
 do
+        newcpu=`head -n1 /proc/stat`
         pw=$(eval $command)
-	prevcpu=`head -n1 /proc/stat`
         en=$(eval $command2)
         curtime=`date +%s`
         curprecise=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)'`
         runtimeprecise=$((curprecise-startprecise))
         runtime=$((curtime-starttime))
-	cpuusage=`printf "$prev \n$new"|awk '/^cpu /{u=$2-u;s=$4-s;i=$5-i;w=$6-w}END{print int(0.5+100*(u+s+w)/(u+s+i+w))}'`
+        cpuusage=`printf "$prevcpu\n$newcpu"|awk '/^cpu /{u=$2-u;s=$4-s;i=$5-i;w=$6-w}END{print int(0.5+100*(u+s+w)/(u+s+i+w))}'`
         j=$(awk "BEGIN {print $en*3600; exit}"|sed "s#,#.#g")
-	echo `date` `date "+%s"` $runtimeprecise ms $pw  W $en Wh $j J $cpuupsage cpu%
+        totcpu=$((`nproc`*$cpuusage))
+        echo `date` `date "+%s"` $runtimeprecise ms $pw  W $en Wh $j J $totcpu cpu% `nproc` hostThreads
+        prevcpu=`head -n1 /proc/stat`
 	sleep $delay
-	newcpu=`head -n1 /proc/stat`
 done	
