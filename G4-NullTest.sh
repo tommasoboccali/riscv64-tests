c3225f97a49083d7031c81eea4de5fc963304b90
Tommaso Boccali <tommaso.boccali@gmail.com>
2024-08-08 09:07:07 +0200

    add nulltest

---
+++ G4-NullTest.sh
@@ -0,0 +1,16 @@
+exe=/usr/bin/sleep
+events_per_thread=100
+prefix=G4_NullTest
+
+echo Starting at `date`
+
+for th in 1 
+do
+	echo executing NullTest \(sleep\) ParFullCMS at ${th} threads and $events_per_thread events at `date`  `date '+%s'`
+	./measure_power.sh 1 "NullTest_${th}" > measure_NullTest_${th}.log &
+	mpid=$!
+	echo Measuring power with PID $mpid
+	time  $exe $events_per_thread>& ${prefix}_${th}.log
+	kill $mpid
+done
+
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ G4: all executed with 1 2 4 8 16 32 64 threads
   G4-ParFullCMS.sh: the full CMS geometry
   
   G4-AnaEx01.sh: the extended  /analysis 01 example
+
+  G4-NullTest.sh: just sleeps for 100 sec to get baseline (runs only at 1 thread)
