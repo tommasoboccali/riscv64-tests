7514a681f5601df8dcaa16c4845b55becabc207b
Tommaso Boccali <tommaso.boccali@cern.ch>
2024-08-08 12:52:28 +0200

    db12

---
+++ db12.sh
@@ -0,0 +1,23 @@
+
+exe=`which db12`
+prefix=db12
+
+
+echo Starting at `date`
+
+./measure_power.sh 5 "db12_ALL" > measure_db12_ALL.csv &
+totmpid=$!
+
+
+for th in 'single' 'whole'
+do
+	echo executing db12 with parameter ${th}  at `date`  `date '+%s'`
+	./measure_power.sh 1 "db12_${th}" > measure_db12_${th}.csv &
+	mpid=$!
+	echo Measuring power with PID $mpid
+	time ${exe} ${th}  >& ${prefix}_${th}.log
+	kill $mpid
+done
+
+kill $totmpid
+
